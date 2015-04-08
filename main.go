c3e5022a5c02a2dddddd68e7561526a5e61a507a
pichuio <patrick.dahlke@pichu.io>
2015-04-08 11:10:25 +0200

    image loader implementation

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import ()
+import (
+	"runtime"
+)
 
 func main() {
-
+	//USING ALL CORES OF YOUR MACHINE FOR PARALLEL PROCESSING
+	numcpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(numcpu)
 }
---
+++ sequential/sequential.go
@@ -0,0 +1,15 @@
+package sequential
+
+import (
+	"image"
+)
+
+func error_diffusion_sequential(width, height int, inputImage, outputImage image.Image) {
+
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+
+		}
+	}
+
+}
