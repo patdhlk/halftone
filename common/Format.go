2fe79f56bc292e8caa90f21aa879bbf161bd6d65
pichuio <patrick.dahlke@pichu.io>
2015-04-15 11:07:03 +0200

    image funcs

---
+++ common/Format.go
@@ -0,0 +1,36 @@
+package common
+
+import (
+	"errors"
+)
+
+type Format int
+
+const (
+	JPEG Format = iota
+	PNG
+	GIF
+	TIFF
+	BMP
+)
+
+func (f Format) String() string {
+	switch f {
+	case JPEG:
+		return "JPEG"
+	case PNG:
+		return "PNG"
+	case GIF:
+		return "GIF"
+	/*case TIFF:
+		return "TIFF"
+	case BMP:
+		return "BMP"*/
+	default:
+		return "Unsupported"
+	}
+}
+
+var (
+	ErrUnsupportedFormat = errors.New("imaging: unsupported image format")
+)
