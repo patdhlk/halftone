3b762f4e5a764bb6b23f8919705e747c46f46f39
pichuio <patrick.dahlke@pichu.io>
2015-05-07 08:23:29 +0200

    basic channel infrastructure

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -1,65 +1,37 @@
 package parallel
 
 import (
-	"github.com/pichuio/halftone/algorithm"
+	//"github.com/pichuio/halftone/algorithm"
 	"github.com/pichuio/halftone/common"
 	"image"
-	"log"
-	"runtime"
 	"sync"
 )
 
 var arr *common.Array
-var mutex = &sync.Mutex{}
+var lock = &sync.Mutex{}
+
+type job struct {
+	line []int32 //use a slice when calling this, not an array
+	y    int     //("vertical") line-number
+}
+
+type result struct {
+	line []int32 //use a slice when calling this, not an array
+	y    int     //("vertical") line-number
+}
+
+func jobFactory(ar *common.Array) chan job {
+	jobs := make(chan job)
+	go func() {
+		for y := 0; y < ar.Height; y++ {
+
+		}
+		close(jobs)
+	}()
+	return jobs
+
+}
 
 func RunParallelMainMutex(ar *common.Array, factorErr float64) *image.RGBA {
-	//var k int
-	progress := make([]int32, ar.Height)
-	height := ar.Height
-	width := ar.Width
-	arr = ar
-	output := common.CloneArray(ar)
-	log.Println("CPUs: ", runtime.NumCPU())
-
-	for k := 0; k < runtime.NumCPU(); k++ {
-		go func() {
-			for i := height - 1; i >= 0; i-- {
-
-				mutex.Lock()
-
-				for j := 0; j < width; j++ {
-
-					log.Println(j, i)
-					if arr.Array[j][i] < 128 {
-						output.Array[j][i] = 0
-					} else {
-						output.Array[j][i] = 1
-					}
-					error := arr.Array[j][i] - 255*output.Array[j][i]
-
-					if i+1 < width {
-						arr.Array[j][i+1] += error * 7 / 16
-					}
-					if j+1 < height && i-1 > -1 {
-						arr.Array[j+1][i-1] += error * 3 / 16
-					}
-
-					if j+1 < height {
-						arr.Array[j+1][i] += error * 5 / 16
-					}
-
-					if j+1 < height && i+1 < width {
-						arr.Array[j+1][i+1] += error * 1 / 16
-					}
-
-					progress[i] = int32(j + 1)
-
-					if i == 2 && j+1 < height {
-						mutex.Unlock()
-					}
-				}
-			}
-		}()
-	}
-	return algorithm.ConvertGrayArrayToImage(output)
+	return nil
 }
